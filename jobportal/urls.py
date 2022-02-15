115847c2623fac9e6387c0d88dca14d0853f634b
rsl081 <53944109+rsl081@users.noreply.github.com>
2022-02-15 16:07:02 +0800

    rsl-navbar

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -49,5 +49,6 @@ urlpatterns = [
     path('latest_jobs', latest_jobs, name="latest_jobs"),
     path('user_latestjobs', user_latestjobs, name="user_latestjobs"),
     path('job_detail/<int:pid>', job_detail, name="job_detail"),
+    path('footer', footer, name="footer"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
